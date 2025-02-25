4eccceb61bbd2359a9360e9f053fc225de95d14a
Koromonnnnnnnn <764363@dpsk12.net>
2025-02-25 10:19:43 -0700

    .

--- dungeonRandomizer.py
+++ dungeonRandomizer.py
@@ -1,11 +1,12 @@
 import pygame
 import random
 from hyperspeed import ParticleSystem
-pygame.init()#initializes Pygame
-pygame.display.set_caption("random walk")#sets the window title
-screen = pygame.display.set_mode((800, 800))#creates game screen
-xpos = 400 #start point for x
-ypos = 400 #start point for y
+
+pygame.init()  # initializes Pygame
+pygame.display.set_caption("random walk")  # sets the window title
+screen = pygame.display.set_mode((800, 800))  # creates game screen
+xpos = 400  # start point for x
+ypos = 400  # start point for y
 xdir = 100
 ydir = 100
 
@@ -34,16 +35,17 @@ map = [
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2] ]
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+]
 
-#render section---------------------------------------------
-    
+# render section---------------------------------------------
+
 dir = [1, 2, 3, 4]
 
 # Render section---------------------------------------------
 
-for i in range(10000): 
-    direction = random.choice(dir) 
+for i in range(10000):
+    direction = random.choice(dir)
 
     if direction == 1:
         ypos -= ydir
@@ -51,7 +53,7 @@ for i in range(10000):
         xpos += xdir
     elif direction == 3:
         ypos += ydir
-    elif direction == 4: 
+    elif direction == 4:
         xpos -= xdir
 
     # Start back in the middle if you go off the screen
@@ -59,7 +61,6 @@ for i in range(10000):
         xpos = 400
     if ypos > 800 or ypos < 0:
         ypos = 400
-
 
     particleSystem.update_particles()
     particleSystem.draw_particles()
